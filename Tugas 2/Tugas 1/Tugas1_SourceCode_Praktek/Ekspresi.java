5653bf2acf4321e6d68221f7216bc6f416b0284b
Rivaldi-211 <1302024))21@student.umi.ac.id>
2026-04-11 17:35:01 +0800

    tes

---
+++ Tugas 2/Tugas 1/Tugas1_SourceCode_Praktek/Ekspresi.java
@@ -0,0 +1,24 @@
+/* pemakaian operator kondisional */
+/*Casting menggunakan tipe data Class*/
+/*13020240021
+M. Rivaldi Juliadin
+Jumat,3/4/2026 
+19:00*/
+ public class Ekspresi {
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* KAMUS */
+        int x = 1;
+        int y = 2;
+        /* ALGORITMA */
+        System.out.print("x = "+ x + "\n");
+        System.out.print("y = "+ y + "\n");
+        System.out.print("hasil ekspresi = (x<y)?x:y = " +
+        ((x < y) ? x : y));
+        /*Gunakan dalam kurung "(statemen dan kondisi)" untuk
+        menyatakan satu kesatuan pernyataan*/
+    }
+ }
---
+++ Tugas 2/Tugas 1/Tugas1_SourceCode_Praktek/Incr.java
@@ -0,0 +1,16 @@
+/* Effek dari operator ++ */
+/*13020240021
+M. Rivaldi Juliadin 
+Jumat,3/4/2026
+19:00*/
+ public class Incr {
+    public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    /* Kamus */
+        int i, j;
+        /* Program */
+        i = 3;
+        j = i++;
+        System.out.println ("Nilai i : " + (++i) + "\nNilai j : " + j);
+    }
+ }
